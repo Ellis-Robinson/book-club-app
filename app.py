586de401807039cacfb4560a417169de6ee4155c
Ellis Robinson <ellis.robinson@live.co.uk>
2021-10-11 12:58:42 +0000

    initial commit

---
+++ app.py
@@ -0,0 +1,18 @@
+import os
+from flask import (Flask, flash, render_template,
+                   redirect, request, session, url_for)
+if os.path.exists("env.py"): 
+    import env
+
+app = Flask(__name__)
+
+
+@app.route("/")
+def test():
+    return "Hello, are you working?"
+
+
+if __name__ == "__main__":
+    app.run(host=os.environ.get("IP"),
+            port=int(os.environ.get("PORT")),
+            debug=True)
