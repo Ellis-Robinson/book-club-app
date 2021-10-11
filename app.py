5ff30ff38eb0cb3f28c13a3fbdf3c99828e080b5
Ellis Robinson <ellis.robinson@live.co.uk>
2021-10-11 13:24:32 +0000

    Add requirements.txt file

--- app.py
+++ app.py
@@ -1,7 +1,7 @@
 import os
 from flask import (Flask, flash, render_template,
                    redirect, request, session, url_for)
-if os.path.exists("env.py"): 
+if os.path.exists("env.py"):
     import env
 
 app = Flask(__name__)
