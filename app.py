6746b8fab1cc13af3d39f685e22bc7b42c9e595b
Ellis Robinson <ellis.robinson@live.co.uk>
2021-10-12 13:42:53 +0000

    Add sign_up page and connect to database

--- app.py
+++ app.py
@@ -4,6 +4,7 @@ from flask import (
     redirect, request, session, url_for)
 from flask_pymongo import PyMongo
 from bson.objectid import ObjectId
+from werkzeug.security import generate_password_hash, check_password_hash
 if os.path.exists("env.py"):
     import env
 
@@ -22,6 +23,19 @@ mongo = PyMongo(app)
 def get_books():
     books = mongo.db.books.find()
     return render_template("books.html", books=books)
+
+
+@app.route("/sign_up", methods=["GET", "POST"])
+def sign_up():
+    if request.method == "POST":
+        register = { 
+            "username": request.form.get("username").lower(),
+            "password": generate_password_hash(request.form.get("password"))
+        }
+        mongo.db.users.insert_one(register)
+        flash("User successfully signed up!")
+
+    return render_template("sign_up.html")
 
 
 if __name__ == "__main__":
