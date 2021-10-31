deed217999adaeb4726d8f882596d4936d5356c4
Ellis Robinson <ellis.robinson@live.co.uk>
2021-10-31 19:17:27 +0000

    Create star rating system in review_book.html  and functionality

--- static/js/script.js
+++ static/js/script.js
@@ -3,3 +3,15 @@ $(document).ready(function() {
     $('.collapsible').collapsible();
     $('select').formSelect();
 });
+
+$("label").click(function() {
+    $(this).parent().find("label").css({
+        "background-color": "grey"
+    })
+    $(this).css({
+        "background-color": "gold"
+    });
+    $(this).nextAll().css({
+        "background-color": "gold"
+    });
+});
