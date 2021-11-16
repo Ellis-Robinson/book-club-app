c946be6dc1147b891c5456ee217daa0855470196
Ellis Robinson <ellis.robinson@live.co.uk>
2021-11-16 14:19:13 +0000

    change debug to False

--- app.py
+++ app.py
@@ -1023,4 +1023,4 @@ def unauthorized_access(error):
 if __name__ == "__main__":
     app.run(host=os.environ.get("IP"),
             port=int(os.environ.get("PORT")),
-            debug=True)
+            debug=False)
